1bef8ef74229bdf0427969417a9ccbbc66f1ed90
cenglevs <st087271@student.spbu.ru>
2022-12-19 14:34:03 +0300

    Initial Commit

--- CandyProject-ui/src/app/cart/cart.component.ts
+++ CandyProject-ui/src/app/cart/cart.component.ts
@@ -28,4 +28,21 @@ export class CartComponent implements OnInit {
   clearCart(){
     this.cartService.clearCart();
   }
+
+  quantityUp(item:any) {
+    item.quantity = item.quantity + 1;
+    item.total = +(item.total + item.price).toFixed(2);
+    this.grandTotal = +(this.grandTotal + item.price).toFixed(2);
+  }
+
+  quantityDown(item:any) {
+    if (item.quantity > 0){
+      item.quantity = item.quantity - 1;
+      item.total = +(item.total - item.price).toFixed(2);
+      this.grandTotal = +(this.grandTotal - item.price).toFixed(2);
+    }
+    else{
+      item.total = 0;
+    }
+  }
 }
